0dca293de135996a4fe3656560c403bf98da7646
rafaelsmedina <r.salesmf@gmail.com>
2017-03-30 21:22:49 -0300

    Adds visualization for graph

--- Lollapalooza Network/network.py
+++ Lollapalooza Network/network.py
@@ -74,6 +74,17 @@ def write_pickle_graph(artist_graph, title):
 def read_pickle_graph(title):
 	return nx.read_gpickle(title + ".gpickle")
 
+def turn_graph_into_visualization_data(artist_graph):
+	nodes = []
+	for artist in artist_graph.nodes():
+		nodes.append({"name": artist})
+	edges = []
+	for edge in artist_graph.edges():
+		artist1, artist2 = edge
+		if artist1 != artist2:
+			edges.append({"source": artist1, "target": artist2})
+	return nodes, edges
+
 #PRINT
 def print_network(artist_graph):
 	G = artist_graph
