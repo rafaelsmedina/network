e708c27c45ca18a3937f667d2a9aea4f22bca0b8
rafaelsmedina <r.salesmf@gmail.com>
2017-04-06 23:58:24 -0300

    Refactors network

--- Lollapalooza Network/app/network.py
+++ Lollapalooza Network/app/network.py
@@ -1,45 +1,47 @@
-from spotify_access import get_related_artists_from_name
+import spotify_access as sa
 import networkx as nx
-from file_manager import read_artists_file, write_pickle_graph, read_pickle_graph,turn_graph_into_visualization_data, print_network
+import file_manager as fm
 
-#NETWORK
-def create_artist_network(artist_name, artist_graph, lollapalooza_artists):
-	print 'Now working on...' + artist_name
-	related_artists_list = get_related_artists_from_name(artist_name)
-	if related_artists_list != None:
-		for related_artist in related_artists_list:
-			if related_artist.encode('utf-8') in lollapalooza_artists:
-				related_artist = related_artist.encode('utf-8')
-				artist_graph.add_edge(artist_name, related_artist)
-
-	artist_graph.add_node(artist_name)
+#create connections
+def connect_artist_to_neighbours(artist_name, artist_graph, artists_list=None, visited_artists=None):
+	if ((visited_artists != None) and (artist_name not in visited_artists)) or (visited_artists == None):
+		print 'Now working on...' + artist_name
+		related_artists_list = sa.get_related_artists_from_name(artist_name)
+		if related_artists_list != None:
+			for related_artist in related_artists_list:
+				if (artists_list == None) or (related_artist.encode('utf-8') in artists_list):
+					related_artist = related_artist.encode('utf-8')
+					artist_graph.add_edge(artist_name, related_artist)
+		artist_graph.add_node(artist_name)
 	return artist_graph
 
-
-def expand_network(artist_graph, lollapalooza_artists):
-	artists_list = list(artist_graph.nodes())
-	for artist in artists_list:
-		if artist in lollapalooza_artists:
-			create_artist_network(artist, artist_graph, lollapalooza_artists)
-	return artist_graph
-
-
-def create_network(artist_name, lollapalooza_artists):
+#get artist first degree connections
+def create_network_from_artist_name(artist_name):
 	artist_graph = nx.Graph()
-	artist_graph = expand_network(create_artist_network(artist_name, artist_graph, lollapalooza_artists), lollapalooza_artists)
+	visited_artists = []
+	artist_graph = connect_artist_to_neighbours(artist_name, artist_graph)
+	visited_artists.append(artist_name)
+	return artist_graph, visited_artists
+
+#get next connections
+def expand_network(artist_graph, visited_artists=None):
+	artists_in_graph = list(artist_graph.nodes())
+	for artist in artists_in_graph:
+		connect_artist_to_neighbours(artist, artist_graph, None, visited_artists)
+		if visited_artists != None:
+			visited_artists.append(artist)
+	return artist_graph, visited_artists
+
+#find adjacent nodes
+def artist_network(artist_name, iterations=0):
+	artist_graph, visited_artists = create_network_from_artist_name(artist_name)
+	for i in range(0, iterations):
+		artist_graph, visited_artists = expand_network(artist_graph, visited_artists)
 	return artist_graph
 
-def create_network_from_list(lollapalooza_artists):
+#connect artists in list
+def list_network(artists_list):
 	artist_graph = nx.Graph()
-	for artist_name in lollapalooza_artists:
-		artist_graph = create_artist_network(artist_name, artist_graph, lollapalooza_artists)
+	for artist_name in artists_list:
+		artist_graph = connect_artist_to_neighbours(artist_name, artist_graph, artists_list)
 	return artist_graph
-
-#MAIN
-def main():
-	lollapalooza_artists = read_artists_file('static/artists.txt')
-	artist_graph = create_network_from_list(lollapalooza_artists)
-	write_pickle_graph(artist_graph, 'lolla')
-
-if __name__ == "__main__":
-	main()
